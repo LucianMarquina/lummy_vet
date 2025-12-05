466635f35fd75604b1041865338c389fdcd74a83
LucianMarquina <alemarquina789@gmail.com>
2025-12-05 15:00:42 -0500

    feat: Modelo de pacientes (mascotas) creados para la integración y relación con modelos de res.partner y vet.appointment

--- models/vet_patient.py
+++ models/vet_patient.py
@@ -5,12 +5,9 @@ class VetPatient(models.Model):
     _description = 'Modelo de pacientes (mascotas)'
 
     name = fields.Char(string='Nombre de Mascota', help="Nombre de la mascota", required=True)
-    birth_date = Fields.Date(string='Fecha de nacimiento', help='Fecha de nacimiento de la mascota')
-    age_patient = feilds.Float(string='Edad', compute='_compute_age', help='Edad de la mascota')
-    pet_breed = feilds.Char(string='Raza', help='Raza de la mascota')
+    birth_date = fields.Date(string='Fecha de nacimiento', help='Fecha de nacimiento de la mascota')
+    age_pet = fields.Char(string='Edad', compute='_compute_age', help='Edad de la mascota', readonly=False)
+    pet_breed = fields.Char(string='Raza', help='Raza de la mascota')
     image_patient = fields.Binary(string='Imagen', help='Imagen de la mascota')
     partner_id = fields.Many2one('res.partner', string='Dueño', required=True)
     history_ids = fields.One2many('vet.appointment', 'pet_ids', string='Historial de citas')
-    
-
-   
